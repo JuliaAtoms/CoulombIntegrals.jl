fa69e0ff95cbb2708b0be7c8d1ec257a91a81788
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2019-03-20 13:31:16 +0100

    Estimate convergence rates using k-means clustering

--- test/test_convergence_rates.jl
+++ test/test_convergence_rates.jl
@@ -1,4 +1,9 @@
 using PrettyTables
+using Statistics
+using ProgressMeter
+
+using RollingFunctions
+using Clustering
 
 if VERSION < v"1.1-DEV"
     isnothing(::Nothing) = true
@@ -6,10 +11,21 @@ if VERSION < v"1.1-DEV"
 end
 
 function estimate_convergence_rate(ρ, ϵ)
-    i = argmin(abs.(ϵ)) - 1
-    i == 0 && return 0
+    x = log10.(ρ)
+    y = log10.(abs.(ϵ))
 
-    ([log10.(ρ[1:i]) ones(i)] \ log10.(abs.(ϵ[1:i])))[1]
+    f = (x,y) -> ([x ones(length(x))] \ y)[1]
+
+    # Find all slopes for consecutive groups of three errors
+    slopes = rolling(f, x, y, 3)
+    e = findfirst(s -> s < 0, slopes)
+    if e !== nothing
+        slopes = slopes[1:max(1,e-1)]
+    end
+
+    k = 3 # Number of clusters requested
+    fit = kmeans(reshape(slopes, 1, :), k)
+    maximum(fit.centers)
 end
 
 function test_convergence_rates(fun::Function, Ns::AbstractVector{<:Integer},
@@ -22,17 +38,21 @@ function test_convergence_rates(fun::Function, Ns::AbstractVector{<:Integer},
                                 verbosity=1)
     ovs = Iterators.product(other_vectors...)
 
+    progress = Progress(length(ovs))
     convergence_rates = map(enumerate(ovs)) do (i,o)
         data = map(N -> fun(N, o...), Ns) |> d -> vcat(filter(!isnothing,d)...)
-        isempty(data) && return nothing
+        if isempty(data)
+            ProgressMeter.next!(progress)
+            return nothing
+        end
         verbosity > 1 && pretty_table(data, headers)
 
         ρ = data[:,ρcol]
         label = join(string.(filter(c -> !(c isa Function), [o...])), ", ")
-        
+
         plot_fun(i, data)
 
-        map(errors) do (ecol,elabel,eexpected,rateexpected)
+        e = map(errors) do (ecol,elabel,eexpected,rateexpected)
             curerr = data[:,ecol]
             minerr = minimum(abs, curerr)
             γ = estimate_convergence_rate(ρ, curerr)
@@ -44,10 +64,15 @@ function test_convergence_rates(fun::Function, Ns::AbstractVector{<:Integer},
                 (isapprox(γ, rateexpected, atol=convergence_rate_atol) || γ > rateexpected)
             Any[minerr γ pass] # Without Any, pass will be coerced to Float64
         end |> e -> hcat(label, e...)
+
+        ProgressMeter.next!(progress)
+
+        e
     end |> e -> vcat(filter(!isnothing, e)...)
+    println()
 
     passjs = 1 .+ 3*(1:length(errors))
-    
+
     if verbosity > 0
         pass = Highlighter((data,i,j) -> j>1 && mod(j-1,3)==0 && data[i,j],
                            bold = true, foreground = :green)
@@ -61,11 +86,12 @@ function test_convergence_rates(fun::Function, Ns::AbstractVector{<:Integer},
                      vcat("Case", [["$elabel, minδ [$eexpected]", "$elabel, rate [$rateexpected]", "Pass"]
                                    for (_,elabel,eexpected,rateexpected) in errors]...),
                      highlighters=(pass,fail),
-                     formatter=formatter)
+                     formatter=formatter,
+                     crop=:none)
         fails = sum([count(.!convergence_rates[:,j]) for j in passjs])
         num_cases = length(errors)*size(convergence_rates,1)
         println("Number of fails: $fails ≡ $(100.0fails/num_cases)% fail rate")
     end
-    
+
     all([all(convergence_rates[:,j]) for j in passjs])
 end

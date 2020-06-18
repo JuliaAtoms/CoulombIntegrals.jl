d334fca4227cde1cff2b845cf9778315a60b6bac
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2020-06-18 09:18:40 +0200

    Optionally don't apply metric inverse in CoulombRepulsionPotential

--- src/coulomb_repulsion_potential.jl
+++ src/coulomb_repulsion_potential.jl
@@ -7,18 +7,19 @@ struct CoulombRepulsionPotential{PP<:AbstractPoissonProblem,
 end
 
 function CoulombRepulsionPotential(R::CompactBases.BasisOrRestricted,
-                                   poisson::AbstractPoissonProblem)
+                                   poisson::AbstractPoissonProblem;
+                                   apply_metric_inverse=true)
     V̂ = LinearOperator(DiagonalOperator(applied(*, R, poisson.Y)), R)
     r = axes(R,1)
     r⁻¹ = R'*QuasiDiagonal(1 ./ r)*R
     tmp = zeros(eltype(poisson), size(R,2))
-    CoulombRepulsionPotential(poisson, V̂, LinearOperator(r⁻¹, R), tmp)
+    CoulombRepulsionPotential(poisson, V̂, apply_metric_inverse ? LinearOperator(r⁻¹, R) : r⁻¹, tmp)
 end
 
 function CoulombRepulsionPotential(R::CompactBases.BasisOrRestricted,
-                                   k::Number, ::Type{C}=eltype(R)) where C
-    poisson = PoissonProblem(R, k, C)
-    CoulombRepulsionPotential(R, poisson)
+                                   k::Number, ::Type{C}=eltype(R); apply_metric_inverse=true, kwargs...) where C
+    poisson = PoissonProblem(R, k, C; kwargs...)
+    CoulombRepulsionPotential(R, poisson, apply_metric_inverse=apply_metric_inverse)
 end
 
 function Base.copyto!(potential::CoulombRepulsionPotential, ρ::Density)
@@ -27,11 +28,9 @@ function Base.copyto!(potential::CoulombRepulsionPotential, ρ::Density)
     potential
 end
 
-function LinearAlgebra.mul!(y, potential::CoulombRepulsionPotential, x,
-                            α::Number=true, β::Number=false)
+LinearAlgebra.mul!(y, potential::CoulombRepulsionPotential, x, α::Number=true, β::Number=false) =
     mul!(y, potential.r⁻¹,
          mul!(potential.tmp, potential.V̂, x),
          α, β)
-end
 
 export CoulombRepulsionPotential

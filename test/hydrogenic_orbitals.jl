71ee06d773e8b717d33c08d7a75fbdc4a5de731f
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2021-03-26 10:13:31 +0100

    Calculate hydrogen orbitals using ClassicalOrthogonalPolynomials

--- test/hydrogenic_orbitals.jl
+++ test/hydrogenic_orbitals.jl
@@ -2,8 +2,7 @@ using ArnoldiMethod
 import ContinuumArrays.QuasiArrays: AbstractQuasiMatrix
 using LazyArrays
 
-using Polynomials
-using OrthoPoly
+using ClassicalOrthogonalPolynomials
 
 function orb_label((n,ℓ))
     ℓs = "spdfg"[ℓ+1]
@@ -17,30 +16,14 @@ Returns a function of `r` (in atomic units, i.e. Bohr radii) that
 evaluates the _reduced_ hydrogenic radial orbital corresponding to the
 quantum numbers `n` and `ℓ`, and for a nuclear charge of `Z`.
 """
-function hydredwfn(::Type{Tuple}, n,ℓ,Z=1)
+function hydredwfn(n,ℓ,Z=1)
     n > 0 || throw(ArgumentError("Invalid n = $n"))
     0 ≤ ℓ && ℓ<n || throw(ArgumentError("Invalid ℓ = $ℓ"))
 
-    N = √((2Z/n)^3*factorial(n-ℓ-1)/(2n*factorial(n+ℓ)))
+    q = 2Z/n
+    N = √(q^3*factorial(n-ℓ-1)/(2n*factorial(n+ℓ)))
 
-    ρ = Polynomial([0,2Z/n],:r)
-    R = ρ^ℓ*laguerre_assoc(n-ℓ-1,2ℓ+1,ρ)
-    P = Polynomial([0,1],:r)*R
-
-    N,ρ,P
-end
-
-"""
-    hydredwfn(n,ℓ[,Z=1])
-
-Returns a function of `r` (in atomic units, i.e. Bohr radii) that
-evaluates the _reduced_ hydrogenic radial orbital corresponding to the
-quantum numbers `n` and `ℓ`, and for a nuclear charge of `Z`.
-"""
-function hydredwfn(n,ℓ,Z=1)
-    N,ρ,P = hydredwfn(Tuple,n,ℓ,Z)
-
-    r -> N*P(r)*exp(-ρ(r)/2)
+    r -> N*r*(q*r)^ℓ*laguerrel(n-ℓ-1,2ℓ+1,q*r)*exp(-q*r/2)
 end
 
 function get_orbitals(R::B, ℓ::Int, Z, nev::Int,
